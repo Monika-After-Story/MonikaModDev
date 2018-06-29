38f38bfb9f6aa31fe0142288f98601139eca093e
potato <potato@desu.zone>
2018-06-28 23:52:27 -0500

    even better build scripts

--- tools/travis.py
+++ tools/travis.py
@@ -1,5 +1,8 @@
 ## this is for travis to run
 
+import gamedir as GDIR
+GDIR.REL_PATH_GAME = "MonikaModDev/Monika After Story/game/"
+
 import spritechecker as spc
 
 bad_codes = spc.check_sprites(False)
