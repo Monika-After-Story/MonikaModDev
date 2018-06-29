da29e1e450d562ef7b30eac50c2a37e81592a85b
potato <potato@desu.zone>
2018-06-28 23:43:06 -0500

    travis fixes

--- tools/travis.py
+++ tools/travis.py
@@ -2,7 +2,7 @@
 
 import spritechecker as spc
 
-bad_codes = check_sprites(False)
+bad_codes = spc.check_sprites(False)
 
 if len(bad_codes) > 0:
     raise Exception("Invalid sprites found. Run the sprite checker for more info.")
