00c77e34b860055cff2b84a3eec620432c66c500
potato <potato@desu.zone>
2018-06-28 23:40:22 -0500

    travis script i hope

---
+++ tools/travis.py
@@ -0,0 +1,8 @@
+## this is for travis to run
+
+import spritechecker as spc
+
+bad_codes = check_sprites(False)
+
+if len(bad_codes) > 0:
+    raise Exception("Invalid sprites found. Run the sprite checker for more info.")
