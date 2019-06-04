4042b8a45d51ad48ef72eac5c66c5ee63d07e231
potato <potato@desu.zone>
2019-06-03 23:56:00 -0500

    staticsprite object done, time for filtering

--- tools/sprite.py
+++ tools/sprite.py
@@ -10,6 +10,14 @@ class StaticSprite(object):
             False otherwise
     """
     _sprite_map = {
+        "is_lean": {
+            "1": False,
+            "2": False,
+            "3": False,
+            "4": False,
+            "5": True,
+            "6": False,
+        },
         "position": {
             "1": "steepling",
             "2": "crossed",
@@ -17,6 +25,14 @@ class StaticSprite(object):
             "4": "pointright",
             "5": ("def", "def"),
             "6": "down",
+        },
+        "sides": {
+            "1": ("1l", "1r"),
+            "2": ("1l", "2r"),
+            "3": ("2l", "2r"),
+            "4": ("2l", "2r"),
+            "5": ("", ""),
+            "6": ("1l", "1r"),
         },
         "eyes": {
             "e": "normal",
@@ -75,23 +91,42 @@ class StaticSprite(object):
             "x": "disgust",
             "p": "pout",
             "t": "triangle"
-        }
+        },
+        "single": { # everything else will be 3b
+            "a": "3a",
+            "u": "3a",
+        },
+        "head": {
+            # exact eye - eyebrow - mouth match
+            "eua": "a",
+            "eub": "b",
+            "euc": "c",
+            "eud": "d",
+            "eka": "e",
+            "ekc": "f",
+            "ekd": "g",
+            "esc": "h",
+            "esd": "i",
+            "hua": "j",
+            "hub": "k",
+            "hkb": "l", # sdl
+            "lka": "m", # sdl
+            "lkb": "n", # sdl
+            "lkc": "o", # sdl
+            "lkd": "p", # sdl
+            "dsc": "q",
+            "dsd": "r",
+        },
     }
+
+    _tab = " " * 4
 
     def __init__(self, spcode):
         """
         Constructor for a static sprite
         """
-        self.invalid = False
-        self.position = None
-        self.eyes = None
-        self.eyebrows = None
-        self.nose = None
-        self.blush = None
-        self.tears = None
-        self.sweatdrop = None
-        self.emote = None
-        self.mouth = None
+        self._init_props()
+        self.spcode = spcode
 
         self.__process_map = {
             "b": self.__process_blush,
@@ -109,6 +144,164 @@ class StaticSprite(object):
 
         self._rip_sprite(spcode)
         self._set_defaults()
+
+    def __str__(self):
+        """
+        To string. This will create a DynamicDisplayable
+        """
+        if self.invalid:
+            return ""
+
+        cnl = ",\n"
+
+        # add the 100% for sure lines
+        lines = [
+            "image monika ",
+            self.spcode,
+            "_static = DynamicDisplayable(\n",
+            self._tab,
+            "mas_drawmonika",
+            cnl,
+            self._tab,
+            "character=monika_chr",
+            cnl,
+            self._tab,
+            "eyebrows=",
+            self.eyebrows,
+            cnl,
+            self._tab,
+            "eyes=",
+            self.eyes,
+            cnl,
+            self._tab,
+            "nose=",
+            self.nose,
+            cnl,
+            self._tab,
+            "mouth=",
+            self.mouth,
+        ]
+
+        # now for the position lines 
+        if self.is_lean:
+            # leaning uses both arms and lean, as well as single
+            lean, arms = self.position
+            lines.extend([
+                cnl,
+                self._tab,
+                "arms=",
+                arms,
+                cnl,
+                self._tab,
+                "lean=",
+                lean,
+                cnl,
+                self._tab,
+                "single=",
+                self.single
+            ])
+
+        else:
+            # otherwise, only have arms and maybe head/sides
+            lines.extend([
+                cnl,
+                self._tab,
+                "arms=",
+                self.position
+            ])
+
+            if self.head:
+                # yes have head
+                left, right = self.sides
+                lines.extend([
+                    cnl,
+                    self._tab,
+                    "head=",
+                    self.head,
+                    cnl,
+                    self._tab,
+                    "left=",
+                    left,
+                    cnl,
+                    self._tab,
+                    "right=",
+                    right
+                ])
+
+        # now for optional parts
+        if self.blush is not None:
+            lines.extend([
+                cnl,
+                self._tab,
+                "blush=",
+                self.blush
+            ])
+
+        if self.tears is not None:
+            lines.extend([
+                cnl,
+                self._tab,
+                "tears=",
+                self.tears
+            ])
+
+        if self.sweatdrop is not None:
+            lines.extend([
+                cnl,
+                self._tab,
+                "sweat=",
+                self.sweatdrop
+            ])
+
+        if self.emote is not None:
+            lines.extend([
+                cnl,
+                self._tab,
+                "emote=",
+                self.emote
+            ])
+
+        # done, add final paren
+        lines.append("\n)")
+
+        return "".join(lines)
+
+    def spstr(self):
+        """
+        Creates the sprite code string for use in ATL statements.
+        :returns: sp code string. This is form (monika <code>_static)
+        """
+        return "monika " + self.spcode + "_static"
+
+    def _get_smap(self, mainkey, code, defval):
+        """
+        Gets a value from the sprite map
+        :param mainkey: which part of sprite map to get from
+        :param code: the code to lookup
+        :param defval: the default value to use
+        :returns: the value from sprite map or defval
+        """
+        return self._sprite_map.get(mainkey, {}).get(code, defval)
+
+    def _init_props(self):
+        """
+        Sets all starting properties to None
+        """
+        self.invalid = False
+        self.position = None
+        self.eyes = None
+        self.eyebrows = None
+        self.nose = None
+        self.blush = None
+        self.tears = None
+        self.sweatdrop = None
+        self.emote = None
+        self.mouth = None
+        self.is_lean = None
+        self.sides = None
+        self.single = None
+        self.head = None
+        self.spcode = None
 
     def _rip_sprite(self, spcode):
         """
@@ -134,32 +327,46 @@ class StaticSprite(object):
             return
 
         # 0 - position
-        position = self._sprite_map.get(spcode[0], None)
+        poscode = spcode[0]
+        position = self._get_smap("position", poscode, defval)
         if position is None:
             self.invalid = True
             return
         self.position = position
+        self.is_lean = self._get_smap("is_lean", poscode, False)
+        self.sides = self._get_smap("sides", poscode, ("", ""))
 
         # 1 - eyes
-        eyes = self._sprite_map.get(spcode[1], None)
+        eyecode = spcode[1]
+        eyes = self._get_smap("eyes", eyecode, None)
         if eyes is None:
             self.invalid = True
             return
         self.eyes = eyes
 
         # 2 - eyebrows
-        eyebrows = self._sprite_map.get(spcode[2], None)
+        eyebrowcode = spcode[2]
+        eyebrows = self._get_smap("eyebrows", eyebrowcode, None)
         if eyebrows is None:
             self.invalid = True
             return
         self.eyebrows = eyebrows
 
         # -1 - mouth
-        mouth = self._sprite_map.get(spcode[-1], None)
+        mouthcode = spcode[-1]
+        mouth = self._get_smap("mouth", mouthcode, None)
         if mouth is None:
             self.invalid = True
             return
         self.mouth = mouth
+        self.single = self._get_smap("single", mouthcode, "3b")
+
+        # determine the head (for standing)
+        self.head = self._get_smap(
+            "head",
+            eyecode + eyebrowcode + mouthcode,
+            ""
+        )
 
         # now get remaining codes
         spcode = spcode[3:-1]
@@ -193,7 +400,6 @@ class StaticSprite(object):
         """
         Sets defaults of sprites that have defaults
         """
-        # so far, nose is the only default
         if self.nose is None:
             self.nose = "def"
 
@@ -213,7 +419,7 @@ class StaticSprite(object):
 
         # next letter must be blush type
         prefixes.append(spcode[index])
-        blush = self._sprite_map.get("".join(prefixes), None)
+        blush = self._get_smap("blush", "".join(prefixes), None)
         if blush is None:
             return False, 0
 
@@ -237,7 +443,7 @@ class StaticSprite(object):
 
         # next letter must be emote type
         prefixes.append(spcode[index])
-        emote = self._sprite_map.get("".join(prefixes), None)
+        emote = self._get_smap("emote", "".join(prefixes), None)
         if emote is None:
             return False, 0
 
@@ -261,7 +467,7 @@ class StaticSprite(object):
 
         # next letter must be nose type
         prefixes.append(spcode[index])
-        nose = self._sprite_map.get("".join(prefixes), None)
+        nose = self._get_smap("nose", "".join(prefixes), None)
         if nose is None:
             return False, 0
 
@@ -316,7 +522,7 @@ class StaticSprite(object):
 
         # next letter must be the sweatdrop type
         prefixes.append(spcode[index])
-        sweatdrop = self._sprite_map.get("".join(prefixes), None)
+        sweatdrop = self._get_smap("sweat", "".join(prefixes), None)
         if sweatdrop is None:
             return False, 0
 
@@ -340,7 +546,7 @@ class StaticSprite(object):
 
         # next letter must be the tear type
         prefixes.append(spcode[index])
-        tears = self._sprite_map.get("".join(prefixes), None)
+        tears = self._get_smap("tears", "".join(prefixes), None)
         if tears is None:
             return False, 0
     
--- tools/spritemaker.py
+++ tools/spritemaker.py
@@ -6,9 +6,150 @@ import menutils
 
 import spritepuller as spull
 
+from sprite import StaticSprite
+
+
+class FilterSprite(StaticSprite):
+    """
+    A Filter sprite is a version of static sprite used for filtering
+    other static sprites.
+
+    The primary difference is that any of the initial properties can be
+    None
+    """
+    POS = "position"
+    EYE = "eyes"
+    EYB = "eyebrows"
+    NSE = "nose"
+    BLH = "blush"
+    TRS = "tears"
+    SWD = "sweat"
+    EMO = "emote"
+    MTH = "mouth"
+
+    def __init__(self, spcode):
+        """
+        Constructor
+        """
+        self._init_props()
+
+    def filter(self, otherStaticSprite):
+        """
+        Checks if the given static sprite passes the filter for this one
+        """
+        pass
+
+    def set_filter(self, category, code):
+        """
+        Sets a filter point
+        :param category: the filter key to set
+        :param code: the code to lookup
+        """
+        pass
+
+    def __flt_set_pos(self, code):
+        """
+        Sets position filter
+        :param code: the code to lookup
+        """
+        self.position = self._get_smap(self.POS, code, None)
+
+        if self.position is None:
+            self.is_lean = None
+        else:
+            self.is_lean = self._get_smap("is_lean", code, False)
+
+    def __flt_set_eye(self, code):
+        """
+        Sets eye filter
+        :param code: the code to lookup
+        """
+        self.eyes = self._get_smap(self.EYE, code, None)
+
+
+
+
+
+
+def _load_sprites():
+    """
+    Loads sprite code data so this module can use it.
+    NOTE: if None is returnd, treat as failure
+    :returns: dictionary of the following format:
+        [0] - sprite code (without static)
+        [1] - StaticSprite object
+    """
+    sprite_list = []
+
+    # load all static sprites
+    for sprite_filepath in spull.STATIC_CHARTS:
+        with open(os.path.normcase(sprite_filepath), "r") as sprite_file:
+            sprite_list.extend(spull.pull_sprite_list_from_file(
+                sprite_file,
+                True
+            ))
+
+    # generate dict of static sprites
+    sprite_db = {}
+    for sprite_code in sprite_list:
+        sprite_obj = StaticSprite(sprite_code)
+
+        # immediately quit if invalid
+        if sprite_obj.invalid:
+            return None
+
+        # otherwise add
+        sprite_db[sprite_code] = sprite_obj
+
+    return sprite_db
+
+
+
 # TODO: pull sprites from the static charts and store them here
 #   1 - allow user search
 #   2 - allow user sprite make
+
+### runners
+
+def run():
+    """
+    Runs this module (menu related)
+    """
+    # first load all sprites
+    print("Loading sprites...", end="")
+    sprite_db = _load_sprites()
+
+    # abort if failed
+    if sprite_db is None:
+        print("\nERROR in loading sprites. Aborting...")
+        return
+
+    # now sort keys
+    sprite_db_keys = sorted(sprite_db.keys())
+
+    # otherwise success
+    print("DONE")
+
+    choice = menutils.menu(menu_main)
+
+    if choice is None:
+        return
+
+    choice(sprite_db, sprite_db_keys)
+
+
+def run_lstc(sprite_db, sprite_db_keys):
+    """
+    List codes submenu
+    """
+    choice = menutils.menu(menu_lstc)
+
+   
+def run_srch(sprite_db, sprite_db_keys):
+    """
+    Interactively searches for a sprite code
+    """
+    pass
 
 
 
@@ -17,6 +158,13 @@ import spritepuller as spull
 menu_main = [
     ("Sprite Maker", "Option: "),
     ("Search Code", run_srch),
+    ("List Codes", run_lstc),
+    ("Make Sprite", run_mkspr),
 ]
 
-
+menu_lstc = [
+    ("Filter Options", "Option: "),
+    ("Show List", run_lstc_show),
+    ("Show Filters", run_lstc_showfilter),
+    ("Set Filter", run_lstc_setfilter),
+]
