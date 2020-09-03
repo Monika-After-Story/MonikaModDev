946c0e5da718ecfdbaae77043f8ced0867d2b7ef
Herman Semin <scpketer@protonmail.ch>
2020-09-03 19:33:23 +0400

    WIP: Add some missing code

--- Monika After Story/game/python-packages/Xlib/protocol/request.py
+++ Monika After Story/game/python-packages/Xlib/protocol/request.py
@@ -44,3 +44,27 @@ class InternAtom(rq.ReplyRequest):
         rq.Card32('atom'),
         rq.Pad(20),
         )
+
+class GetProperty(rq.ReplyRequest):
+    _request = rq.Struct(
+        rq.Opcode(20),
+        rq.Bool('delete'),
+        rq.RequestLength(),
+        rq.Window('window'),
+        rq.Card32('property'),
+        rq.Card32('type'),
+        rq.Card32('long_offset'),
+        rq.Card32('long_length'),
+        )
+
+    _reply = rq.Struct(
+        rq.ReplyCode(),
+        rq.Format('value', 1),
+        rq.Card16('sequence_number'),
+        rq.ReplyLength(),
+        rq.Card32('property_type'),
+        rq.Card32('bytes_after'),
+        rq.LengthOf('value', 4),
+        rq.Pad(12),
+        rq.PropertyData('value'),
+        )
--- Monika After Story/game/python-packages/Xlib/xobject/drawable.py
+++ Monika After Story/game/python-packages/Xlib/xobject/drawable.py
@@ -19,7 +19,7 @@
 #    Suite 330,
 #    Boston, MA 02111-1307 USA
 
-from Xlib.protocol import request
+from ..protocol import request
 
 from . import resource
 
