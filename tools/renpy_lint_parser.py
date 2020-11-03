50862ad9f89142899ff86ea66c2d2444706e47f7
potato <potato@desu.zone>
2020-11-03 13:48:05 -0600

    add 4

--- tools/renpy_lint_parser.py
+++ tools/renpy_lint_parser.py
@@ -8,7 +8,7 @@ import os
 # regex parsing
 
 IMG_NOT_FOUND = re.compile(
-    "\w+/(\w+/)*.+\.rpy:\d+ (Could not find image \(monika |The image named 'monika )(\d\w\w\w+|\d\w|1|5|g1|g2)"
+    "\w+/(\w+/)*.+\.rpy:\d+ (Could not find image \(monika |The image named 'monika )(\d\w\w\w+|\d\w|1|5|4|g1|g2)"
 )
 
 # file load
