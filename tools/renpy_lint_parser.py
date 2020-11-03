b4296cb5b1942834105c2ee4860e7ab8f6451c69
potato <potato@desu.zone>
2020-11-03 13:40:40 -0600

    include stock images

--- tools/renpy_lint_parser.py
+++ tools/renpy_lint_parser.py
@@ -8,7 +8,7 @@ import os
 # regex parsing
 
 IMG_NOT_FOUND = re.compile(
-    "\w+/(\w+/)*.+\.rpy:\d+ (Could not find image \(monika |The image named 'monika )\d\w\w\w+"
+    "\w+/(\w+/)*.+\.rpy:\d+ (Could not find image \(monika |The image named 'monika )\d\w+"
 )
 
 # file load
